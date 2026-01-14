eeed42957a6b847a4ad4d0bf7e66043aaadfa643
Abir Banerjee <abirbanerjee793@gmail.com>
2026-01-14 10:25:16 +0530

    multi client [client code ]

---
+++ multi_clientChat/client.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#define PORT 8040
+
+void chat(int sockFd){
+    char buffer[500];
+    while(1){
+        // New conversation
+        memset(buffer , 0 , sizeof(buffer));
+        printf("Enter data to be sent to server\n");
+        fgets(buffer , sizeof(buffer) - 1 , stdin);
+
+
+        ssize_t n = sizeof(buffer);
+        buffer[strcspn(buffer , '\n')] = '\0';
+
+        if(strcmp(buffer , "exit") == 0){
+            break;
+        }
+
+        write(sockFd , buffer , n);
+
+        ssize_t r = read(sockFd , buffer , sizeof(buffer) - 1);
+        if(r <= 0){
+            printf('Server closed\n');
+        }
+        
+        buffer[r] = '\0';
+        print('Received string: %s\n' , buffer);
+
+    }
+}
+
+int main(){
+    struct sockaddr_in serverAddr , clientAddr;
+    
+    int sockFd = socket(AF_INET , SOCK_STREAM , 0);
+
+    if (sockFd < 0)
+    {
+        printf("Client socket failed to open\n");
+        exit(1);
+    }
+
+
+    memset(&serverAddr  , 0 ,sizeof(serverAddr));
+    serverAddr.sin_family =AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if(connect(sockFd , (struct sockaddr*)&serverAddr , sizeof(serverAddr)) < 0){
+        perror("Failed to connect\n");
+        exit(1);
+    }
+
+    chat(sockFd);
+    close(sockFd);
+
+    return 0;
+
+}
